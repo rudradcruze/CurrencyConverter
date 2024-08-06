2963b55913f469799268ccc237a9e12fb31ac676
rudradcruze <francisrudra@gmail.com>
2024-08-06 12:14:20 +0600

    logging and swgger ui

--- src/main/java/org/rudradcruze/thirdpartyapi/currencyconvert/client/impl/CurrencyClientImpl.java
+++ src/main/java/org/rudradcruze/thirdpartyapi/currencyconvert/client/impl/CurrencyClientImpl.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.rudradcruze.thirdpartyapi.currencyconvert.client.CurrencyClient;
 import org.rudradcruze.thirdpartyapi.currencyconvert.dto.CurrencySuccessResponseDto;
 import org.rudradcruze.thirdpartyapi.currencyconvert.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
@@ -19,6 +21,7 @@ import java.util.Map;
 public class CurrencyClientImpl implements CurrencyClient {
 
     private final RestClient restClient;
+    Logger log = LoggerFactory.getLogger(CurrencyClientImpl.class);
 
     @Value("${currencyService.api.key}")
     private String apiKey;
@@ -26,18 +29,23 @@ public class CurrencyClientImpl implements CurrencyClient {
     @Override
     public Map<String, Double> getCurrencyConversionRate(String fromCurrency, List<String> toCurrencies) {
         String url = "/latest?apikey=" + apiKey + "&currencies=" + String.join(",", toCurrencies) + "&base_currency=" + fromCurrency;
-        String str = "";
+        log.trace("Trying to retrieve the currency conversion rate in getCurrencyConversionRate");
+        log.trace("URL : {}", url);
         try {
+            log.trace("Attempting to call the restClient Method in getCurrencyConversionRate");
             CurrencySuccessResponseDto apiResponse = restClient.get()
                     .uri(url)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
                         String errorBody = new String(response.getBody().readAllBytes());
+                        log.error("Client Error occurred in getCurrencyConversionRate : {}", errorBody);
                         throw new ResourceNotFoundException(errorBody);
                     })
                     .body(new ParameterizedTypeReference<>() {});
+            log.debug("Successfully retrieved the currency conversion rate in getCurrencyConversionRate");
             return apiResponse.getRates();
         } catch (RuntimeException e) {
+            log.error("Exception occurred in getCurrencyConversionRate", e.getMessage());
             throw new ResourceNotFoundException(e.getMessage());
         }
     }
